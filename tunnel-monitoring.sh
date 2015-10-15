7c5a0ae3da489a131aa0ae5d63135dc1ce269de3
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-10-15 08:25:38 +0200

    Add script for setting up tunnels to monitoring master

---
+++ tunnel-monitoring.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+. ./vocabulary.sh
+
+eval `ssh-agent`
+ssh-add
+
+wait_begin
+
+ssh $SSH_OPTS -L 8080:localhost:8080 -N root@$(server_external_ip $COLLECTD_HOST) &
+wait_add $!
+
+sudo -E ssh $SSH_OPTS -L 80:localhost:80 -N root@$(server_external_ip $COLLECTD_HOST) &
+wait_add $!
+
+wait_join
