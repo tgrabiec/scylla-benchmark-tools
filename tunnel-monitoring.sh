aae1a822ea8176c9894983274642ad64cc6ade0b
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-10-19 13:17:42 +0200

    tunnel-monitoring: Add tunnel for riemann server

--- tunnel-monitoring.sh
+++ tunnel-monitoring.sh
@@ -7,6 +7,9 @@ ssh-add
 
 wait_begin
 
+ssh $SSH_OPTS -L 5556:localhost:5556 -N root@$(server_external_ip $COLLECTD_HOST) &
+wait_add $!
+
 ssh $SSH_OPTS -L 8080:localhost:8080 -N root@$(server_external_ip $COLLECTD_HOST) &
 wait_add $!
 
